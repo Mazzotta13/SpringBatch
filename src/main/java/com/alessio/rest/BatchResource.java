efeeb7101144bc9b2ce9ef987a84678bf047911a
Alessio Mazzotta <alessio.mazzotta@digitalmetrics@eu>
2020-01-29 14:12:19 +0100

    lanch job in a rest controller

---
+++ src/main/java/com/alessio/rest/BatchResource.java
@@ -0,0 +1,32 @@
+package com.alessio.rest;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BatchResource {
+	
+	@Autowired
+    private JobLauncher jobLauncher;
+	
+	@Autowired
+	private Job importUserJob;
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/startBatch")
+	public void startBatch() throws Exception {
+		jobLauncher.run(importUserJob, new JobParameters());
+	}
+}
